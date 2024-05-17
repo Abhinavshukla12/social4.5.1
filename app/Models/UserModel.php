59e304576427c33161732c9a898145f2cb68f577
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-05-17 19:01:34 +0530

    try to fix validation error

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,13 +6,13 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    protected $table = 'users';
+    protected $table = 'user';
     protected $primarykey = 'id';
     protected $allowedFields = ['name', 'email']; // Fields that can be mass-assigned
 
     // You can define validation rules here or in the controller
     protected $validationRules = [
-        'name' => 'required|min_length[5]|max_length[10]',
+        'name' => 'required|min_length[4]|max_length[10]',
         'email' => 'required|valid_email'
     ];
 }
